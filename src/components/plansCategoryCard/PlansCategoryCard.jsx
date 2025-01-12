7cff8a851553ade33faf2a643aacaaf0aa6286a1
HuzaifaSidiqui <huzaifa.siddique5000@gmail.com>
2025-01-12 14:37:06 +0500

    categories card adjustment for extra large screens

--- src/components/plansCategoryCard/PlansCategoryCard.jsx
+++ src/components/plansCategoryCard/PlansCategoryCard.jsx
@@ -25,28 +25,28 @@ export default function PlansCategoryCard({ color, title, img, id, language }) {
           sm: 180,
           md: 200,
           lg: 250,
-          xl: 320,
+          xl: 300,
         },
         minWidth: {
           xs: 140,
           sm: 180,
           md: 200,
           lg: 250,
-          xl: 320,
+          xl: 300,
         },
         maxHeight: {
           xs: 184,
           sm: 225,
           md: 250,
           lg: 300,
-          xl: 388,
+          xl: 350,
         },
         minHeight: {
           xs: 184,
           sm: 225,
           md: 250,
           lg: 300,
-          xl: 388,
+          xl: 350,
         },
         borderRadius: {
           xs: "12px",
@@ -54,7 +54,7 @@ export default function PlansCategoryCard({ color, title, img, id, language }) {
           md: "24px",
           lg: "24px",
         },
-        border: "2px solid",
+        border: "4px solid",
         borderColor: color,
         flexGrow: 1,
         cursor: "pointer",
@@ -76,7 +76,7 @@ export default function PlansCategoryCard({ color, title, img, id, language }) {
               sm: 158,
               md: 175,
               lg: 215,
-              xl: 280,
+              xl: 250,
             },
             width: "100%",
             objectFit: "cover",
@@ -92,7 +92,7 @@ export default function PlansCategoryCard({ color, title, img, id, language }) {
               sm: 158,
               md: 175,
               lg: 215,
-              xl: 280,
+              xl: 250,
             },
             width: "100%",
             objectFit: "cover",
@@ -114,25 +114,25 @@ export default function PlansCategoryCard({ color, title, img, id, language }) {
             lg: 1.5,
             xl: 2,
           },
-          gap: {
-            xs: 0.5,
-            sm: 1,
-            md: 1,
-            lg: 1,
-            xl: 2.5,
-          },
+          // gap: {
+          //   xs: 0.5,
+          //   sm: 1,
+          //   md: 1,
+          //   lg: 1,
+          //   xl: 2.5,
+          // },
         }}
       >
         <Typography
           variant="body"
-          component="div"
+          // component="div"
           sx={{
             fontSize: {
               xs: "0.75rem",
               sm: "1rem",
               md: "1.25rem",
               lg: "1.5rem",
-              xl: "1.75rem",
+              xl: "1.5rem",
             },
             fontWeight: "bold",
             textAlign: isRTL ? "right" : "left",
@@ -149,7 +149,7 @@ export default function PlansCategoryCard({ color, title, img, id, language }) {
               sm: "0.85rem",
               md: "1rem",
               lg: "1rem",
-              xl: "1.25rem",
+              xl: "1rem",
             },
             textAlign: isRTL ? "right" : "left",
           }}
